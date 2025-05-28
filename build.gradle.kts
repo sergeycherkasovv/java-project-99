21f15e342ed45da587981b41e55a21b1c2737db9
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-05-28 15:20:47 +0700

    changed build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
