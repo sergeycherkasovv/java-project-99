7923647212620c2ad38208ad1d459ac342170d91
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-05-30 16:22:27 +0700

    add plugins and Spring components in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	application
 	checkstyle
 	jacoco
+	id("io.freefair.lombok") version "8.13.1"
 	id("org.sonarqube") version "6.2.0.5505"
 	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
@@ -28,9 +29,17 @@ repositories {
 }
 
 dependencies {
+	// Spring Boot
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// DB
+	runtimeOnly("com.h2database:h2")
+
+	// Tests
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
