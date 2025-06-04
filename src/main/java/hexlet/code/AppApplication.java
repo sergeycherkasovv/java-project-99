d890c9b9c377753e55751a4a4d3ce5079c6f280f
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-04 08:42:12 +0700

    add bean 'getFaker()' and annotation 'EnableJpaAuditing' to AppApplication.java

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,13 +1,22 @@
 package hexlet.code;
 
+import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @Bean
+    public Faker getFaker() {
+        return new Faker();
     }
 
 }
