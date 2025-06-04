d90e29a3f5cddbfb12c3312b1a0c67c6374c0406
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-04 08:38:25 +0700

    create UserCreateDTO.java

---
+++ src/main/java/hexlet/code/dto/user/UserCreateDTO.java
@@ -0,0 +1,23 @@
+package hexlet.code.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateDTO {
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    private String email;
+
+    @Size(min = 3, max = 100)
+    private String password;
+}
