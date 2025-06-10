0a0029e555947592a51cfb7e35e1f3ea37c3bfe4
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-10 11:07:45 +0700

    changed UserCreateDTO.java

--- src/main/java/hexlet/code/dto/user/UserCreateDTO.java
+++ src/main/java/hexlet/code/dto/user/UserCreateDTO.java
@@ -2,6 +2,7 @@ package hexlet.code.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +19,7 @@ public class UserCreateDTO {
     @Email
     private String email;
 
+    @NotNull
     @Size(min = 3, max = 100)
     private String password;
 }
