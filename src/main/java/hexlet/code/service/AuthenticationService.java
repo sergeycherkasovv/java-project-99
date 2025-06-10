f73a2811df8406a069b03a6b19270947423331d4
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-10 11:10:35 +0700

    create AuthenticationService.java

---
+++ src/main/java/hexlet/code/service/AuthenticationService.java
@@ -0,0 +1,26 @@
+package hexlet.code.service;
+
+import hexlet.code.model.AuthRequest;
+import hexlet.code.util.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public String createAuthentication(AuthRequest authRequest) {
+        var authentication = new UsernamePasswordAuthenticationToken(
+                authRequest.getUsername(), authRequest.getPassword());
+
+        authenticationManager.authenticate(authentication);
+
+        return jwtUtils.generateToken(authRequest.getUsername());
+    }
+}
