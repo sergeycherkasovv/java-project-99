9aaf381ee949e01b0daa9550398640d37afd1918
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-12 19:11:50 +0700

    create TaskStatusService.java

---
+++ src/main/java/hexlet/code/service/TaskStatusService.java
@@ -0,0 +1,53 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.taskStatus.TaskStatusCreateDTO;
+import hexlet.code.dto.taskStatus.TaskStatusDTO;
+import hexlet.code.dto.taskStatus.TaskStatusUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.TaskStatusMapper;
+import hexlet.code.repository.TaskStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskStatusService {
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private TaskStatusMapper taskStatusMapper;
+
+    public List<TaskStatusDTO> getAllTaskStatus() {
+        var taskStatus = taskStatusRepository.findAll();
+        var taskStatusDTOList = taskStatus.stream()
+                .map(taskStatusMapper::map)
+                .toList();
+        return taskStatusDTOList;
+    }
+
+    public TaskStatusDTO findByIdTaskStatus(Long id) {
+        var taskStatus = taskStatusRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("TaskStatus with id " + id + " not found"));
+        return taskStatusMapper.map(taskStatus);
+    }
+
+    public TaskStatusDTO createTaskStatus(TaskStatusCreateDTO taskStatusData) {
+        var taskStatus = taskStatusMapper.map(taskStatusData);
+        taskStatusRepository.save(taskStatus);
+        return taskStatusMapper.map(taskStatus);
+    }
+
+    public TaskStatusDTO updateTaskStatus(TaskStatusUpdateDTO taskStatusData, Long id) {
+        var taskStatus = taskStatusRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("TaskStatus with id " + id + " not found"));
+        taskStatusMapper.update(taskStatusData, taskStatus);
+        taskStatusRepository.save(taskStatus);
+        return taskStatusMapper.map(taskStatus);
+    }
+
+    public void deleteTaskStatus(Long id) {
+        taskStatusRepository.deleteById(id);
+    }
+}
