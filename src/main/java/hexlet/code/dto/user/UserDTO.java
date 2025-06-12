d63facd8707638248057bbedc752ff7ad4c57ca0
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-12 19:10:01 +0700

    delete @JsonFormat to UserDTO.java

--- src/main/java/hexlet/code/dto/user/UserDTO.java
+++ src/main/java/hexlet/code/dto/user/UserDTO.java
@@ -1,6 +1,5 @@
 package hexlet.code.dto.user;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +12,5 @@ public class UserDTO {
     private String username;
     private String firstName;
     private String lastName;
-    //@JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate createdAt;
 }
