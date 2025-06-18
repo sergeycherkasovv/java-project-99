cc5e94e913467ea19e6b4678481365ae811d0349
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-18 15:48:32 +0700

    changed imports to UserMapper.java

--- src/main/java/hexlet/code/mapper/UserMapper.java
+++ src/main/java/hexlet/code/mapper/UserMapper.java
@@ -4,7 +4,13 @@ import hexlet.code.dto.user.UserCreateDTO;
 import hexlet.code.dto.user.UserDTO;
 import hexlet.code.dto.user.UserUpdateDTO;
 import hexlet.code.model.User;
-import org.mapstruct.*;
+import org.mapstruct.BeforeMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
