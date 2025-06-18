1e2d34cde9d8178107aa3fe52dada831210607db
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-18 15:40:22 +0700

    create TaskCreateDTO.java

---
+++ src/main/java/hexlet/code/dto/task/TaskCreateDTO.java
@@ -0,0 +1,24 @@
+package hexlet.code.dto.task;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskCreateDTO {
+    private Integer index;
+
+    @JsonProperty("assignee_id")
+    private Long assigneeId;
+
+    @NotBlank
+    private String title;
+
+    private String content;
+
+    @NotNull
+    private String status;
+}
