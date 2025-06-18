ce49187e7a11b145b251de1a1a738a681f5eb556
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-18 15:46:49 +0700

    changed imports to AuthenticationController.java

--- src/main/java/hexlet/code/controller/api/AuthenticationController.java
+++ src/main/java/hexlet/code/controller/api/AuthenticationController.java
@@ -3,7 +3,10 @@ package hexlet.code.controller.api;
 import hexlet.code.model.AuthRequest;
 import hexlet.code.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/login")
