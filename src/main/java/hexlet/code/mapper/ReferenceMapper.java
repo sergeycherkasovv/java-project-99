0286e00834eacdb3ee488c5a2d25c96a0ea3f609
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-18 15:19:25 +0700

    add method 'toEntityBySlug()' to ReferenceMapper.java

--- src/main/java/hexlet/code/mapper/ReferenceMapper.java
+++ src/main/java/hexlet/code/mapper/ReferenceMapper.java
@@ -1,9 +1,13 @@
 package hexlet.code.mapper;
 
+import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.model.BaseEntity;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.TaskStatusRepository;
 import jakarta.persistence.EntityManager;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
 import org.mapstruct.TargetType;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,7 +18,18 @@ public abstract class ReferenceMapper {
     @Autowired
     private EntityManager entityManager;
 
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
     public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) {
         return id != null ? entityManager.find(entityClass, id) : null;
+    }
+
+    @Named("toEntityBySlug")
+    public TaskStatus toEntityBySlug(String slug) {
+        return slug != null
+                ? taskStatusRepository
+                    .findBySlug(slug).orElseThrow(() -> new ResourceNotFoundException("Not slug: " + slug))
+                : null;
     }
 }
