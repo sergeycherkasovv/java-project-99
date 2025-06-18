fe6682959bdeeb324e7191b61d7575aaea835625
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-18 15:22:10 +0700

    changed TaskStatusControllerTest.java

--- src/test/java/hexlet/code/controller/api/TaskStatusControllerTest.java
+++ src/test/java/hexlet/code/controller/api/TaskStatusControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -94,8 +93,7 @@ class TaskStatusControllerTest {
 
     @Test
     void testCreate() throws Exception {
-        var data = Instancio.of(modelGenerator.getTaskStatusModel())
-                .create();
+        var data = Instancio.of(modelGenerator.getTaskStatusModel()).create();
 
         var request = post("/api/task_statuses")
                 .contentType(MediaType.APPLICATION_JSON)
