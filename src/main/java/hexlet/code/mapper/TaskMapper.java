c917618edde098a06c78e60b17a602b8acbe73b1
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-18 15:19:40 +0700

    create TaskMapper.java

---
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -0,0 +1,36 @@
+package hexlet.code.mapper;
+
+import hexlet.code.dto.task.TaskCreateDTO;
+import hexlet.code.dto.task.TaskDTO;
+import hexlet.code.dto.task.TaskUpdateDTO;
+import hexlet.code.model.Task;
+import org.mapstruct.*;
+
+@Mapper(
+        uses = {JsonNullableMapper.class, ReferenceMapper.class} ,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskMapper {
+
+    @Mapping(source = "name", target = "title")
+    @Mapping(source = "description", target = "content")
+    @Mapping(source = "assignee.id", target = "assigneeId")
+    @Mapping(source = "taskStatus.slug", target = "status")
+    public abstract TaskDTO map(Task model);
+
+    @Mapping(source = "assigneeId", target = "assignee.id")
+    @Mapping(source = "title", target = "name")
+    @Mapping(source = "content", target = "description")
+    @Mapping(source = "status", target = "taskStatus", qualifiedByName = "toEntityBySlug")
+    public abstract Task map(TaskCreateDTO dto);
+
+    public abstract void update(TaskUpdateDTO dto, @MappingTarget Task model);
+
+    @Mapping(source = "assigneeId", target = "assignee.id")
+    @Mapping(source = "title", target = "name")
+    @Mapping(source = "content", target = "description")
+    @Mapping(source = "status", target = "taskStatus", qualifiedByName = "toEntityBySlug")
+    public abstract Task map(TaskDTO dto);
+}
