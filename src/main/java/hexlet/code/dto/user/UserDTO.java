5f2c52e2f76aaa6c0544d1e7431a45d7577dd8db
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-18 15:18:54 +0700

    changed UserDTO.java

--- src/main/java/hexlet/code/dto/user/UserDTO.java
+++ src/main/java/hexlet/code/dto/user/UserDTO.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Setter
 public class UserDTO {
     private Long id;
-    private String username;
+    private String email;
     private String firstName;
     private String lastName;
     private LocalDate createdAt;
