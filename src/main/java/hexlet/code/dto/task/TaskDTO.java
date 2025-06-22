0a727cc27a214fac5b0a521f7a2f9a690dfbb63a
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-22 16:11:01 +0700

    changed TaskDTO.java

--- src/main/java/hexlet/code/dto/task/TaskDTO.java
+++ src/main/java/hexlet/code/dto/task/TaskDTO.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,4 +24,6 @@ public class TaskDTO {
     private String content;
 
     private String status;
+
+    private List<Long> taskLabelIds;
 }
