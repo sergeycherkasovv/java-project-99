a85cc005ba5487d234ad852c082f039ddb4be301
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-22 18:27:46 +0700

    add field 'labels' to Task.java

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
@@ -16,6 +17,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -45,6 +47,9 @@ public class Task implements BaseEntity {
 
     @ManyToOne
     private User assignee;
+
+    @ManyToMany
+    private List<Label> labels;
 
     @CreatedDate
     private LocalDate createdAt;
