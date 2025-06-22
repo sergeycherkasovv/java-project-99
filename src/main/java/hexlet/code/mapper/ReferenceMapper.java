f69f7359d12c00de01271b7de641dc3f8296fa67
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-22 16:12:03 +0700

    add methods to ReferenceMapper.java

--- src/main/java/hexlet/code/mapper/ReferenceMapper.java
+++ src/main/java/hexlet/code/mapper/ReferenceMapper.java
@@ -2,7 +2,9 @@ package hexlet.code.mapper;
 
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.model.BaseEntity;
+import hexlet.code.model.Label;
 import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskStatusRepository;
 import jakarta.persistence.EntityManager;
 import org.mapstruct.Mapper;
@@ -10,6 +12,9 @@ import org.mapstruct.MappingConstants;
 import org.mapstruct.Named;
 import org.mapstruct.TargetType;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mapper(
         componentModel = MappingConstants.ComponentModel.SPRING
@@ -21,15 +26,40 @@ public abstract class ReferenceMapper {
     @Autowired
     private TaskStatusRepository taskStatusRepository;
 
+    @Autowired
+    private LabelRepository labelRepository;
+
     public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) {
         return id != null ? entityManager.find(entityClass, id) : null;
     }
 
-    @Named("toEntityBySlug")
-    public TaskStatus toEntityBySlug(String slug) {
+    @Named("toTaskStatusBySlug")
+    public TaskStatus toTaskStatusBySlug(String slug) {
         return slug != null
                 ? taskStatusRepository
                     .findBySlug(slug).orElseThrow(() -> new ResourceNotFoundException("Not slug: " + slug))
                 : null;
     }
+
+    @Named("toLabelsById")
+    public List<Label> toLabelsById(List<Long> labelsId) {
+        return labelsId == null || labelsId.isEmpty()
+                ? new ArrayList<>()
+                : labelsId.stream().map(this::getLabelById).toList();
+    }
+
+    @Named("toIdByLabels")
+    public List<Long> toIdByLabels(List<Label> labels) {
+        return labels == null || labels.isEmpty()
+                ? new ArrayList<>()
+                : labels.stream().map(Label::getId)
+                .toList();
+    }
+
+    private Label getLabelById(Long id) {
+        return labelRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Label with id " + id + " not found"));
+    }
+
+
 }
