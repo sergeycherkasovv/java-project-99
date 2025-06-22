60184a13ef28e7771fba3f2a4cf401f9e4fd4136
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-22 18:48:12 +0700

    delete import TaskStatus.java

--- src/main/java/hexlet/code/model/TaskStatus.java
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -11,7 +11,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
