4df06076fb79b12d96edb32cbd1e796d2ec5334b
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-24 13:00:35 +0700

    change UserRepository.java

--- src/main/java/hexlet/code/repository/UserRepository.java
+++ src/main/java/hexlet/code/repository/UserRepository.java
@@ -2,12 +2,11 @@ package hexlet.code.repository;
 
 import hexlet.code.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
