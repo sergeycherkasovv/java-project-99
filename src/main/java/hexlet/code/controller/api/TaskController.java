a5aa4458e4495ecb7de48f6a57b3d715644a9ce2
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-24 12:57:59 +0700

    change TaskController.java

--- src/main/java/hexlet/code/controller/api/TaskController.java
+++ src/main/java/hexlet/code/controller/api/TaskController.java
@@ -2,21 +2,14 @@ package hexlet.code.controller.api;
 
 import hexlet.code.dto.task.TaskCreateDTO;
 import hexlet.code.dto.task.TaskDTO;
+import hexlet.code.dto.task.TaskParamsDTO;
 import hexlet.code.dto.task.TaskUpdateDTO;
 import hexlet.code.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,8 +20,8 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping
-    ResponseEntity<List<TaskDTO>> index() {
-        var taskDTOList = taskService.getAllTask();
+    ResponseEntity<List<TaskDTO>> index(TaskParamsDTO params) {
+        var taskDTOList = taskService.getAllTask(params);
         return ResponseEntity.ok()
                 .header("X-Total-Count", String.valueOf(taskDTOList.size()))
                 .body(taskDTOList);
