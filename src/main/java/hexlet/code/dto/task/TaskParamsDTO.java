466f3ddb9b5334699abcf6f2d653bdc7a26334b5
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-24 12:59:04 +0700

    create TaskParamsDTO.java

---
+++ src/main/java/hexlet/code/dto/task/TaskParamsDTO.java
@@ -0,0 +1,16 @@
+package hexlet.code.dto.task;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskParamsDTO {
+    private String titleCont;
+
+    private Long assigneeId;
+
+    private String status;
+
+    private Long labelId;
+}
