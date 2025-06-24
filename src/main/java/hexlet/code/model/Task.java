c5d647687f0a3f2b50db403f453fd839a0d3d247
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-24 13:00:05 +0700

    change Task.java

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -17,7 +17,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -49,7 +50,7 @@ public class Task implements BaseEntity {
     private User assignee;
 
     @ManyToMany
-    private List<Label> labels;
+    private Set<Label> labels = new HashSet<>();
 
     @CreatedDate
     private LocalDate createdAt;
