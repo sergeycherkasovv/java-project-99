5324333118f52aab3608a7a601eb9c77dbf3c7dc
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-24 13:01:55 +0700

    change LabelControllerTest.java

--- src/test/java/hexlet/code/controller/api/LabelControllerTest.java
+++ src/test/java/hexlet/code/controller/api/LabelControllerTest.java
@@ -14,12 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -32,6 +32,7 @@ import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles(value = "development")
 class LabelControllerTest {
     private final String url = "/api/labels";
     private final String urlId = "/api/labels/{id}";
