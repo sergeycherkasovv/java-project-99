6b6a64712019a073f1741023b18f3294ae44c9a5
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-24 12:59:44 +0700

    change TaskMapper.java

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -33,6 +33,11 @@ public abstract class TaskMapper {
     @Mapping(source = "taskLabelIds", target = "labels", qualifiedByName = "toLabelsById")
     public abstract Task map(TaskCreateDTO dto);
 
+    @Mapping(source = "assigneeId", target = "assignee.id")
+    @Mapping(source = "title", target = "name")
+    @Mapping(source = "content", target = "description")
+    @Mapping(source = "status", target = "taskStatus", qualifiedByName = "toTaskStatusBySlug")
+    @Mapping(source = "taskLabelIds", target = "labels", qualifiedByName = "toLabelsById")
     public abstract void update(TaskUpdateDTO dto, @MappingTarget Task model);
 
     @Mapping(source = "assigneeId", target = "assignee.id")
