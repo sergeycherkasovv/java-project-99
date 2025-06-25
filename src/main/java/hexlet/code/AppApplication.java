85e1acc98f1b5f5ab76f70bdb52de516b7cbe24b
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-25 18:36:27 +0700

    changed AppApplication.java

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -6,17 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.lang.Exception;
+import io.sentry.Sentry;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class AppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
+        try {
+            SpringApplication.run(AppApplication.class, args);
+            throw new Exception("This is a test.");
+        } catch (Exception e) {
+            Sentry.captureException(e);
+        }
     }
 
     @Bean
     public Faker getFaker() {
         return new Faker();
     }
-
 }
