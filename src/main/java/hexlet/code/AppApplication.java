102581f781f84d4ebff407a3799ad2a214a33a52
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-25 19:14:37 +0700

    changed AppApplication.java

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -6,20 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.lang.Exception;
-import io.sentry.Sentry;
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class AppApplication {
 
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(AppApplication.class, args);
-            throw new Exception("This is a test.");
-        } catch (Exception e) {
-            Sentry.captureException(e);
-        }
+        SpringApplication.run(AppApplication.class, args);
     }
 
     @Bean
