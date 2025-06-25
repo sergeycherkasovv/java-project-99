d54e4b72cdcdacedad0e537c981595f74f9ae0f2
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-26 00:10:18 +0700

    changed Label.java

--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -1,12 +1,10 @@
 package hexlet.code.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -17,9 +15,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -42,7 +37,4 @@ public class Label {
 
     @CreatedDate
     private LocalDate createdAt;
-
-    @ManyToMany(mappedBy = "labels", cascade = CascadeType.MERGE)
-    private Set<Task> tasks = new HashSet<>();
 }
