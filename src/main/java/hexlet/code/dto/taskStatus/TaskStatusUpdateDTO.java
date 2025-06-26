1f817dd2ef4f98adafefef7fe4cd51d839690d43
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-26 18:01:21 +0700

    changed TaskStatusUpdateDTO.java

--- src/main/java/hexlet/code/dto/taskStatus/TaskStatusUpdateDTO.java
+++ src/main/java/hexlet/code/dto/taskStatus/TaskStatusUpdateDTO.java
@@ -1,6 +1,5 @@
 package hexlet.code.dto.taskStatus;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
@@ -8,9 +7,7 @@ import org.openapitools.jackson.nullable.JsonNullable;
 @Getter
 @Setter
 public class TaskStatusUpdateDTO {
-    @NotNull
     private JsonNullable<String> slug;
 
-    @NotNull
     private JsonNullable<String> name;
 }
