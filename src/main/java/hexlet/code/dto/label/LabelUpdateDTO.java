0f89661ffdebb6a86b44a9b8c401e3fae54d9b99
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-26 18:09:22 +0700

    deleted import to LabelUpdateDTO.java

--- src/main/java/hexlet/code/dto/label/LabelUpdateDTO.java
+++ src/main/java/hexlet/code/dto/label/LabelUpdateDTO.java
@@ -1,7 +1,6 @@
 package hexlet.code.dto.label;
 
 import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
