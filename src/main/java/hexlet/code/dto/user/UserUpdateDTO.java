61d08c1c1af4bbac0b9d0fcb2121fb58401cafda
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-26 18:01:29 +0700

    changed UserUpdateDTO.java

--- src/main/java/hexlet/code/dto/user/UserUpdateDTO.java
+++ src/main/java/hexlet/code/dto/user/UserUpdateDTO.java
@@ -1,7 +1,6 @@
 package hexlet.code.dto.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.openapitools.jackson.nullable.JsonNullable;
 import lombok.Getter;
@@ -10,10 +9,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserUpdateDTO {
-    @NotBlank
+
     private JsonNullable<String> firstName;
 
-    @NotBlank
+
     private JsonNullable<String> lastName;
 
     @Email
