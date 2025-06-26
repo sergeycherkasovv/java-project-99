6485b167db8d083f0c7dca0155b7c53e7e289cc6
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-26 13:36:38 +0700

    changed LabelControllerTest.java

---
+++ Makefile
@@ -0,0 +1,27 @@
+clean:
+	./gradlew clean
+
+test:
+	./gradlew test
+
+reload-classes:
+	./gradlew -t classes
+
+start-prod:
+	./gradlew bootRun --args='--spring.profiles.active=prod'
+
+install:
+	./gradlew installDist
+
+lint:
+	./gradlew checkstyleMain checkstyleTest
+
+help:
+	@echo "make start-prod      - запустить Spring Boot в продакшн-профиле"
+	@echo "make install         - собрать самодостаточный дистрибутив"
+	@echo "make clean   		- удалить артефакты сборки"
+	@echo "make test    		- выполнить все тесты"
+	@echo "make lint            - выполнить проверку кода через Checkstyle"
+	@echo "make reload-classes  - авто-пересборка классов при изменениях"
+
+.PHONY: test clean reload-classes start-prod install lint help
--- src/test/java/hexlet/code/controller/api/LabelControllerTest.java
+++ src/test/java/hexlet/code/controller/api/LabelControllerTest.java
@@ -21,7 +21,10 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.StandardCharsets;
