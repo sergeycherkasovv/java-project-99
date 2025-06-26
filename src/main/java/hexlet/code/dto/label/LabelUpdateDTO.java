dbff7739b322234ac3b0ac121c82b817dfb7b1ec
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-26 18:00:58 +0700

    changed LabelUpdateDTO.java

--- src/main/java/hexlet/code/dto/label/LabelUpdateDTO.java
+++ src/main/java/hexlet/code/dto/label/LabelUpdateDTO.java
@@ -10,7 +10,6 @@ import org.openapitools.jackson.nullable.JsonNullable;
 @Getter
 @Setter
 public class LabelUpdateDTO {
-    @NotNull
     @Size(min = 3, max = 1000)
     @Column(unique = true)
     private JsonNullable<String> name;
