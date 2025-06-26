d85c2c343422843188a3ef56ae6326e34a76b7b6
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-26 18:01:09 +0700

    changed TaskUpdateDTO.java

--- src/main/java/hexlet/code/dto/task/TaskUpdateDTO.java
+++ src/main/java/hexlet/code/dto/task/TaskUpdateDTO.java
@@ -1,8 +1,6 @@
 package hexlet.code.dto.task;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
@@ -17,12 +15,10 @@ public class TaskUpdateDTO {
     @JsonProperty("assignee_id")
     private JsonNullable<Long> assigneeId;
 
-    @NotBlank
     private JsonNullable<String> title;
 
     private JsonNullable<String> content;
 
-    @NotNull
     private JsonNullable<String> status;
 
     private JsonNullable<Set<Long>> taskLabelIds;
