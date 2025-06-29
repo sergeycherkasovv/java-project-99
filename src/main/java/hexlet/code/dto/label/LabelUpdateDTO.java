12003c10b8e6ac82530f93fbba3b733a587a6797
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-29 20:43:59 +0700

    deleted annotation'@Column' to LabelUpdateDTO.java

--- src/main/java/hexlet/code/dto/label/LabelUpdateDTO.java
+++ src/main/java/hexlet/code/dto/label/LabelUpdateDTO.java
@@ -1,6 +1,5 @@
 package hexlet.code.dto.label;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +9,5 @@ import org.openapitools.jackson.nullable.JsonNullable;
 @Setter
 public class LabelUpdateDTO {
     @Size(min = 3, max = 1000)
-    @Column(unique = true)
     private JsonNullable<String> name;
 }
