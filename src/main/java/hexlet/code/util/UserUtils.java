843614e24eb98293a60278ad962057244aa20f4d
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-30 00:51:45 +0700

    changed UserUtils.java

--- src/main/java/hexlet/code/util/UserUtils.java
+++ src/main/java/hexlet/code/util/UserUtils.java
@@ -13,6 +13,7 @@ public class UserUtils {
 
     public User getCurrentUser() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
+
         if (authentication == null || !authentication.isAuthenticated()) {
             return null;
         }
