33ff5885025ee4091729e24712c76a388005aa2f
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-06-29 23:02:40 +0700

    Moved methods from ReferenceMapper.java to TaskMapper.java

--- src/main/java/hexlet/code/mapper/ReferenceMapper.java
+++ src/main/java/hexlet/code/mapper/ReferenceMapper.java
@@ -1,21 +1,11 @@
 package hexlet.code.mapper;
 
-import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.model.BaseEntity;
-import hexlet.code.model.Label;
-import hexlet.code.model.TaskStatus;
-import hexlet.code.repository.LabelRepository;
-import hexlet.code.repository.TaskStatusRepository;
 import jakarta.persistence.EntityManager;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.Named;
 import org.mapstruct.TargetType;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Mapper(
         componentModel = MappingConstants.ComponentModel.SPRING
@@ -24,47 +14,8 @@ public abstract class ReferenceMapper {
     @Autowired
     private EntityManager entityManager;
 
-    @Autowired
-    private TaskStatusRepository taskStatusRepository;
-
-    @Autowired
-    private LabelRepository labelRepository;
-
     public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) {
         return id != null ? entityManager.find(entityClass, id) : null;
     }
 
-    @Named("toTaskStatusBySlug")
-    public TaskStatus toTaskStatusBySlug(String slug) {
-        return slug != null
-                ? taskStatusRepository
-                    .findBySlug(slug)
-                    .orElseThrow(() -> new ResourceNotFoundException("Not slug: " + slug))
-                : null;
-    }
-
-    @Named("toLabelsById")
-    public Set<Label> toLabelsById(Set<Long> labelsId) {
-        return labelsId == null || labelsId.isEmpty()
-                ? new HashSet<>()
-                : labelsId
-                    .stream()
-                    .map(this::getLabelById)
-                    .collect(Collectors.toSet());
-    }
-
-    @Named("toIdByLabels")
-    public Set<Long> toIdByLabels(Set<Label> labels) {
-        return labels == null || labels.isEmpty()
-                ? new HashSet<>()
-                : labels
-                    .stream()
-                    .map(Label::getId)
-                    .collect(Collectors.toSet());
-    }
-
-    private Label getLabelById(Long id) {
-        return labelRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Label with id " + id + " not found"));
-    }
 }
--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -3,13 +3,25 @@ package hexlet.code.mapper;
 import hexlet.code.dto.task.TaskCreateDTO;
 import hexlet.code.dto.task.TaskDTO;
 import hexlet.code.dto.task.TaskUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.model.Label;
 import hexlet.code.model.Task;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.LabelRepository;
+import hexlet.code.repository.TaskStatusRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @Mapper(
         uses = {JsonNullableMapper.class, ReferenceMapper.class},
@@ -18,6 +30,11 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class TaskMapper {
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private LabelRepository labelRepository;
 
     @Mapping(source = "name", target = "title")
     @Mapping(source = "description", target = "content")
@@ -46,4 +63,31 @@ public abstract class TaskMapper {
     @Mapping(source = "status", target = "taskStatus", qualifiedByName = "toTaskStatusBySlug")
     @Mapping(source = "taskLabelIds", target = "labels", qualifiedByName = "toLabelsById")
     public abstract Task map(TaskDTO dto);
+
+    @Named("toTaskStatusBySlug")
+    public TaskStatus toTaskStatusBySlug(String slug) {
+        return slug != null
+                ? taskStatusRepository
+                .findBySlug(slug)
+                .orElseThrow(() -> new ResourceNotFoundException("Not slug: " + slug))
+                : null;
+    }
+
+    @Named("toLabelsById")
+    public Set<Label> toLabelsById(Set<Long> labelsId) {
+        return labelsId == null || labelsId.isEmpty()
+                ? new HashSet<>()
+                : labelRepository.findAllById(labelsId)
+                                    .stream()
+                                    .collect(Collectors.toSet());
+    }
+
+    @Named("toIdByLabels")
+    public Set<Long> toIdByLabels(Set<Label> labels) {
+        return labels == null || labels.isEmpty()
+                ? new HashSet<>()
+                : labels.stream()
+                    .map(Label::getId)
+                    .collect(Collectors.toSet());
+    }
 }
